0ca23d96e503b46ff74a799335f9fd953edf1adb
Elias Chane <elias.chane@eaglelionsystems.com>
2025-10-27 16:57:32 +0300

    payment service update

---
+++ services/payment-service/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message);
+});
+
+app.post("/payment-service", async (req, res) => {
+  const { cart } = req.body;
+
+  const userId = "123";
+
+  //TODO: PAYMENT
+
+  console.log("API endpoint hit!");
+
+  //KAFKA
+
+  return res.status(200).send("Payment Successful!");
+});
+
+app.listen(8000, () => {
+  console.log("Payment service running on port 8000");
+});
